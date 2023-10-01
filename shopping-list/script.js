9fcf238e61a937b68a1e5310710c1a5c87b4f2dc
Sean Brook <sbrook776@gmail.com>
2023-10-02 10:40:57 +1300

    add items and clear items

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -1,18 +1,22 @@
 const itemForm = document.querySelector("#item-form");
 const itemInput = document.querySelector("#item-input");
 const itemList = document.querySelector("#item-list");
+const clearBtn = document.querySelector("#clear");
+const itemFilter = document.querySelector("#filter");
 
 function submitInput(e) {
 	e.preventDefault();
 	const newItem = itemInput.value;
-	if (newItem.value === "") {
-		console.log("Please enter an item");
+	if (newItem === "") {
+		alert("Please enter an item");
 		return;
 	}
 	const li = document.createElement("li");
 	li.appendChild(document.createTextNode(newItem));
 	const button = itemButton("remove-item btn-link text-red");
+	// Add li to the DOM
 	itemList.appendChild(li);
+	checkUI();
 	li.appendChild(button);
 	itemInput.value = "";
 }
@@ -31,5 +35,36 @@ function createIcon(classes) {
 	return icon;
 }
 
+function removeItem(e) {
+	if (e.target.parentElement.classList.contains("remove-item")) {
+		if (confirm("Are you sure you want to delete this item?")) {
+			e.target.parentElement.parentElement.remove();
+			checkUI();
+		}
+	}
+}
+function clearItems() {
+	while (itemList.firstChild) {
+		itemList.removeChild(itemList.firstChild);
+	}
+	checkUI();
+}
+
+function checkUI() {
+	const items = itemList.querySelectorAll("li");
+
+	if (items.length === 0) {
+		clearBtn.style.display = "none";
+		itemFilter.style.display = "none";
+	} else {
+		clearBtn.style.display = "block";
+		itemFilter.style.display = "block";
+	}
+}
+
 //Event listeners
 itemForm.addEventListener("submit", submitInput);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
+
+checkUI();
