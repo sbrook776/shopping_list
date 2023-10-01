27279d77159f008aa0ece115ca8b1f9ad35b3bfc
Sean Brook <sbrook776@gmail.com>
2023-10-02 09:40:47 +1300

    working add items

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -3,17 +3,18 @@ const itemInput = document.querySelector("#item-input");
 const itemList = document.querySelector("#item-list");
 
 function submitInput(e) {
-	const newItem = itemInput;
 	e.preventDefault();
+	const newItem = itemInput.value;
 	if (newItem.value === "") {
 		console.log("Please enter an item");
 		return;
 	}
 	const li = document.createElement("li");
-	itemList.appendChild(li);
 	li.appendChild(document.createTextNode(newItem));
-	itemButton("remove-item btn-link text-red");
+	const button = itemButton("remove-item btn-link text-red");
+	itemList.appendChild(li);
 	li.appendChild(button);
+	itemInput.value = "";
 }
 
 function itemButton(classes) {
